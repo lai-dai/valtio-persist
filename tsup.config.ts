74aad3cbea7d7d5a1354f31b489851cfdc2e0531
laidai <laidai9966@gmail.com>
2024-04-10 10:36:36 +0700

    update package

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'tsup'
+import { defineConfig } from "tsup";
 
 export default defineConfig({
-  target: 'es2020',
-  format: ['cjs', 'esm'],
+  target: "es2020",
+  format: ["cjs", "esm"],
+  entry: ["src/index.ts"],
   splitting: false,
   sourcemap: true,
   clean: true,
-  dts: true
-})
+  dts: true,
+});
